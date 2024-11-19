3d64b492247e6e31ab0ead095c7374e1512bf6d0
Tamim S <75170424+TamGamer97@users.noreply.github.com>
2024-11-19 15:02:55 +0000

    User ID Generation

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/Premier-League-Table', async(req, res) => {
 
 app.get('/generate-user-id/:email', async(req, res) => { // figure out how to pass in email to request
   const email = req.params.email; // Access the route parameter
+  console.log(email)
   res.send(await generateUniqueUserId(email))
 })
 
